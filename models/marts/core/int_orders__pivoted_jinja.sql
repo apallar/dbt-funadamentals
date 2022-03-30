94087991eea642122681b123a62d3b14e8f3e744
Ann Pallar <annrhizapallar@yahoo.com>
2022-03-30 22:58:08 +0000

    test

--- models/marts/core/int_orders__pivoted_jinja.sql
+++ models/marts/core/int_orders__pivoted_jinja.sql
@@ -1,4 +1,5 @@
 {%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
 with payments as (
     select * from {{ ref('stg_payments') }}
 ),
@@ -6,12 +7,12 @@ with payments as (
 pivoted as (
     select
         order_id,
-        {%- for payment_method in payment_methods -%}
+        {% for payment_method in payment_methods %}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
         {%- if not loop.last -%}
-          ,
-        {%- endif -%}
-        {%- endfor -%}
+            ,
+        {%- endif %}
+        {% endfor -%}
 
     from payments
     where status = 'success'
